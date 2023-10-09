0d1c8d7aae3a5f0a770a60da03ce2986a84346d6
Christopher <Chris.Medina93@gmail.com>
2023-10-09 08:40:11 -0700

    added profile && displayed bookmarks in profile

---
+++ src/Profile.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = ({auth, bookmarks})=>{
+    return(
+        <div>
+            <h2>{auth.username}</h2><br/>
+            <hr/>
+            <h4>Bookmarked Items</h4>
+            <ul>
+                {
+                    bookmarks.map(bookmark =>{
+                        return(
+                            <li key = { bookmark.id }>
+                                { bookmark.product_name}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+};
+
+export default Profile;
